6a56a3d97eb2ff803c015ca03661e69d5c75366f
chichiri <chichiri>
2024-04-17 16:01:07 +0800

    u

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -9,11 +9,15 @@ public class GameManager : SingleBase<GameManager>
     [SerializeField] private GameObject mainBuilding;
     [SerializeField] private GameStatus gameStatus = GameStatus.Padding;
 
+    private bool isPause = false;
+
     public event EventHandler OnPrepareStart;
     public event EventHandler OnPrepareEnd;
     public event EventHandler OnBattleStart;
     public event EventHandler OnBattleEnd;
     public event EventHandler OnGameStatusUpdate;
+    public event EventHandler OnPause;
+    public event EventHandler OnRegain;
 
     public Player GetPlayer()
     {
@@ -50,5 +54,24 @@ public class GameManager : SingleBase<GameManager>
         OnPrepareEnd?.Invoke(this, EventArgs.Empty);
         UpdateGameStatus(GameStatus.Battle);
         OnBattleStart?.Invoke(this, EventArgs.Empty);
+    }
+
+    //暂停游戏
+    public void Pause()
+    {
+        isPause = true;
+        Time.timeScale = 0;
+        OnPause?.Invoke(this, EventArgs.Empty);
+    }
+
+    //恢复游戏
+    public void Regain()
+    {
+        if (isPause)
+        {
+            isPause = false;
+            Time.timeScale = 1;
+            OnRegain?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
